98a0890ed1261f60d67c7a3f962cb288b1492dcb
Tanish <tanishkumar5007@gmail.com>
2022-01-05 03:47:03 +0530

    :)

---
+++ app/src/main/java/com/example/mymovieapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.mymovieapp
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mymovieapp.navigation.MovieNavigation
+import com.example.mymovieapp.ui.theme.MyMovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            MyApp {
+                MovieNavigation()
+            }
+
+
+        }
+    }
+}
+//Container Function
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+
+    MyMovieAppTheme {
+        content()
+    }
+
+}
+
+
+
+@Preview(showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+
+    MyApp {
+        MovieNavigation()git
+    }
+
+}
---
+++ app/src/main/java/com/example/mymovieapp/navigation/MovieNagivation.kt
@@ -0,0 +1,22 @@
+package com.example.mymovieapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mymovieapp.screens.home.HomeScreen
+
+@Composable
+fun MovieNavigation(){
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = MovieScreens.HomeScreen.name
+    ){
+        composable(MovieScreens.HomeScreen.name){
+            //Here we pass where this should lead us to
+            HomeScreen(navController = navController)
+        }
+    }
+}
